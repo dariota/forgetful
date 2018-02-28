39aec34d0682280afaec3b71e94c5f5424ff8a1b
Dário Tavares Antunes <dario@ntun.es>
2018-02-28 16:36:38 +0000

    Detect occurences of malloc/free not in assignment

--- plugin/visit.ml
+++ plugin/visit.ml
@@ -1,55 +1,46 @@
 open Cil_types
 
-let print_stmt out = function
-    | Instr i -> Printer.pp_instr out i
-    | Return ((Some e),_) -> Format.fprintf out "<return> %a " Printer.pp_exp e
-    | Return (None,_) -> Format.pp_print_string out "<return>"
-    | Goto _ -> Format.pp_print_string out "<goto> "
-    | Break _ -> Format.pp_print_string out "<break> "
-    | Continue _ -> Format.pp_print_string out "<continue> "
-    | If (e,_,_,_) -> Format.fprintf out "if %a" Printer.pp_exp e
-    | Switch (e,_,_,_) -> Format.fprintf out "switch %a" Printer.pp_exp e
-    | Loop _ -> Format.fprintf out "<loop> "
-    | Block _ -> Format.fprintf out "<block> "
-    | UnspecifiedSequence _ -> Format.fprintf out "<unspec> "
+let opt_string = function
+    | None   -> "None"
+    | Some s -> s
 
-let printer_str func input = func Format.str_formatter input; Format.flush_str_formatter ()
+let rec lhost_contains_dyn_mem (l : lhost) : bool = match l with
+    | Var v -> Options.feedback ~level:2 "Var";
+               Options.feedback ~level:2 "varinfo: %s %s %s %b" v.vname v.vorig_name (opt_string v.vdescr) v.vdescrpure;
+               v.vorig_name = "malloc" || v.vorig_name = "free"
+    | _     -> false
+    and exp_contains_dyn_mem (e : exp_node) (s : stmt) : bool = match e with
+    | Lval (h,_) -> Options.feedback ~level:3 "Lval";
+                    lhost_contains_dyn_mem h
+    | _          -> Options.feedback ~level:3 "wuzz other";
+                    false
 
-let rec exp_contains_free (e : exp) : bool = match e.enode with
-    | UnOp (_,e,_) -> exp_contains_free e
-    | BinOp (_,e1,e2,_) -> exp_contains_free e || exp_contains_free e
-    | CastE (_,e) -> exp_contains_free e
-    | Info (e,_) -> exp_contains_free e
-    | _ -> false
+let instr_contains_dyn_mem (i : instr) (s : stmt) : bool = match i with
+    | Call (_,e,es,_) -> Options.feedback ~level:4 "Call";
+                         ignore (Options.feedback ~level:4 "e: %a" Printer.pp_exp e);
+                         exp_contains_dyn_mem e.enode s
+                         || (Options.feedback ~level:4 "list";
+                             List.fold_left (||) false (List.map (fun e -> Options.feedback ~level:4 "l: %a" Printer.pp_exp e; exp_contains_dyn_mem e.enode s) es))
+    | Local_init _ -> Options.feedback ~level:4 "Local_init";
+                      (* getting back to this later true; *)
+                      false
+    | _            -> false
 
-let rec stmt_contains_free (s : stmtkind) : bool = match s with
-    | Instr i -> true
-    | Return (e,_) -> true
-    | If (e,bi,be,_) -> true
-    | Switch (e,_,slst,_) -> true
-    | Loop (_,bc,_,scnt,sbrk) -> true
-    | Block b -> true
-    | UnspecifiedSequence us -> stmt_contains_free (Block (Cil.block_from_unspecified_sequence us))
-    | _ -> false
+let stmt_contains_dyn_mem (s : stmt) : bool = match s.skind with
+    | Instr i -> instr_contains_dyn_mem i s
+    | _       -> false
 
 class print_cfg out = object
     inherit Visitor.frama_c_inplace
 
-    method !vfile _ =
-        Format.fprintf out "@[< hov 2> digraph cfg {@ ";
-        Cil.DoChildrenPost (fun f -> Format.fprintf out "}@]@."; f)
-
     method !vglob_aux g =
         match g with
-        | GFun (f,_) ->
-                Options.result "func: %s" f.svar.vname;
-                Format.fprintf out "@[< hov 2> subgraph cluster_%a {@ \
-                                    @[< hv 2> graph@ [label=\"%a\"];@]@ "
-                    Printer.pp_varinfo f.svar Printer.pp_varinfo f.svar;
-                Cil.DoChildrenPost (fun g -> Format.fprintf out "}@]@ "; g)
-        | _ -> Cil.SkipChildren
+        | GFun (f,_) -> Options.feedback ~level:6 "Processing function %s" f.svar.vname;
+                        Cil.DoChildrenPost (fun g -> g)
+        | _          -> Cil.SkipChildren
 
-    method! vstmt_aux s =
+    method! vstmt_aux s = Options.feedback ~level:5 "Processing statement %a" Printer.pp_stmt s;
+        (* (* Leaving this here as sample use of EVA *)
         let color =
             if Db.Value.is_computed () then
                 let state = Db.Value.get_stmt_state s in
@@ -63,18 +54,12 @@ class print_cfg out = object
         in
             Format.fprintf out "@[s%d@ [label=%S %s]@];@ " s.sid (Pretty_utils.to_string print_stmt s.skind) color;
             List.iter (fun succ -> Format.fprintf out "@[s%d -> s%d;@]@ " s.sid succ.sid) s.succs;
+        *)
         let _ =
-            match s.skind with
-            | Instr i -> ignore (match i with
-                         | Call (_,e,es,_) -> ignore (Options.result "<call> %a"  Printer.pp_exp e);
-                                              ignore (List.iter (fun e -> Options.result "<calls> %a " Printer.pp_exp e) es)
-                         (*
-                          * Looks like mallocs happen in this next one somewhere as opposed to as a call
-                          * This is a result of mallocs always (at least if they're useful) being assigned.
-                          * Can still warn on an unassigned malloc...
-                          * *)
-                         | _ -> ignore (Options.result "not interesting %a" Printer.pp_instr i))
-            | _ -> ignore (Options.result "also not interesting"); ignore (Options.result "%s" (printer_str Printer.pp_stmt s))
+            if stmt_contains_dyn_mem s then
+                Options.result "Found statement with dynamic memory %a" Printer.pp_stmt s
+            else
+                ();
         in
             Cil.DoChildren
 end
