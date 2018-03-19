8ddf4799b5ccf511783c55b0aec968e7ee552b89
Dário Tavares Antunes <dario@ntun.es>
2018-03-19 11:40:38 +0000

    Comment the results parsing, add results files

--- src/naive/parse.py
+++ src/naive/parse.py
@@ -1,5 +1,7 @@
 import csv
 
+# taking in some collection of values, leave only 4 or 5 inner values,
+# removing outliers depending on whether the source list was even or odd
 def dump_outliers(values):
     length = len(values)
     if length < 5:
@@ -11,11 +13,20 @@ def dump_outliers(values):
 
 csvs = {}
 
+# read in the 4 different results files for the various optimisation levels
 for i in range(0, 4):
     csvs["O{}".format(i)] = csv.reader(open("resultsO{}.csv".format(i)))
 
 results = {}
 
+# parse each csv into dicts
+# { "olevel": # O0, O1 etc.
+#   { "alloc_type": # Dynamic, stack, etc
+#     { "item_count": # 1, 8, 16, etc
+#       [ <times> ]
+#     }
+#   }
+# }
 for olevel in csvs:
     results[olevel] = {}
     for row in csvs[olevel]:
@@ -33,6 +44,17 @@ for olevel in csvs:
 
 computed = {}
 
+# transform the preceding dict into
+# { "olevel":
+#   { "item_count":
+#     {
+#       "avgs": [ (<time>, "alloc_type"), ... ],
+#       "mins": [ (<time>, "alloc_type"), ... ],
+#       "maxs": [ (<time>, "alloc_type"), ... ]
+#     }
+#   }
+# }
+# however, when calculating the avgs/mins/maxs, dump the outliers first
 for olevel in results:
     computed[olevel] = {}
     for memtype in results[olevel]:
@@ -53,6 +75,8 @@ for olevel in results:
             i_stats["maxs"].append((max(current), memtype))
             i_stats["mins"].append((min(current), memtype))
 
+# produce sorted lists indicating the relative performance of each method
+# the first item in each list was the fastest and so forth
 for olevel in computed:
     for i_count in computed[olevel]:
         for category in computed[olevel][i_count]:
