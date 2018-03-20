d4a2e200f3562a4d3a7265bd55ac2f5b6ab54c6b
Dário Tavares Antunes <dario@ntun.es>
2018-03-20 17:44:25 +0000

    Add parallel benchmark with way more mallocs
    Simply keeps mallocing for a predetermined time period, copying things
    into the allocated array. Doesn't perform properly above O1 as the
    allocations and busy work are optimised out (except in the external
    case) since the allocated array is unreachable.

---
+++ src/parallel/main.c
@@ -0,0 +1,150 @@
+#include <stdio.h>
+#include <time.h>
+#include <pthread.h>
+#include <sys/time.h>
+
+#include "benchmark.h"
+
+#define TIME_SEC 2
+
+union function_option {
+	void (*bench_func)(size_t,bench_item*);
+	void (*extra_bench_func)(size_t,bench_item*,bench_item*);
+};
+
+struct bench_info {
+	volatile int *done;
+	volatile int *start;
+	size_t len;
+	bench_item *items;
+	bench_item *extra_items;
+	union function_option func;
+};
+
+void *worker(void *arg) {
+	struct bench_info *info = arg;
+	while (!(*info->start));
+	int complete_count = 0;
+
+	// explicit hoisting to be sure the common expression's handled
+	size_t len = info->len;
+	bench_item *items = info->items;
+	bench_item *extra_items = info->extra_items;
+
+	// explicit duplication to avoid branching in core loop
+	if (info->extra_items == NULL) {
+		for (; !(*info->done); complete_count++) {
+			(*info->func.bench_func)(len, items);
+		}
+	} else {
+		for (; !(*info->done); complete_count++) {
+			(*info->func.extra_bench_func)(len, items, extra_items);
+		}
+	}
+
+	// explicit conversions to avoid warnings
+	return (void*) (long int) complete_count;
+}
+
+void show_time(const char * description, size_t items, struct timeval start, struct timeval end, long int actions) {
+	int secs = end.tv_sec - start.tv_sec;
+	int usecs = end.tv_usec - start.tv_usec;
+	if (usecs < 0) {
+		secs -= 1;
+		usecs += 1000000;
+	}
+
+	printf("%s,%u,%f,%ld\n", description, items, (secs + (usecs / 1000000.0)), actions);
+}
+
+void run_benchmark(const char *description, size_t *lens, bench_item **items, bench_item **extra_items, union function_option bench_func) {
+	for (int i = 0; i < 8; i++) {
+		volatile int done = 0;
+		volatile int start = 0;
+		pthread_t threads[4];
+		struct bench_info info[4];
+		for (int j = 0; j < 4; j++) {
+			info[j].done = &done;
+			info[j].start = &start;
+			info[j].len = lens[i];
+			info[j].items = items[i];
+			info[j].extra_items = extra_items[i];
+			info[j].func = bench_func;
+			pthread_create(&threads[j], NULL, worker, &info[j]);
+		}
+
+		start = 1;
+		struct timeval start_t;
+		gettimeofday(&start_t, NULL);
+		struct timeval target_t;
+		target_t.tv_sec = start_t.tv_sec + TIME_SEC;
+		struct timeval current_t;
+		do {
+			gettimeofday(&current_t, NULL);
+		} while (current_t.tv_sec < target_t.tv_sec || (current_t.tv_sec == target_t.tv_sec && current_t.tv_usec < start_t.tv_usec));
+		done = 1;
+		gettimeofday(&current_t, NULL);
+
+		long int total = 0;
+		for (int j = 0; j < 4; j++) {
+			void *retval = 0;
+			pthread_join(threads[j], &retval);
+			total += (long int) retval;
+		}
+		show_time(description, lens[i], start_t, current_t, total);
+	}
+}
+
+int main(int argc, char *argv[]) {
+	long int runs;
+	if (argc == 1) {
+		runs = 10000L;
+	} else if (argc == 2) {
+		char *endptr = NULL;
+		runs = strtol(argv[1], &endptr, 10);
+		
+		if (endptr == NULL) {
+			fprintf(stderr, "Input '%s' is not a valid number\n", argv[1]);
+			return 2;
+		}
+	} else {
+		printf("Usage: %s [runs]\n"
+		       "\tRuns the benchmark, calling each function 'runs' times, or 10000 times if omitted.\n",
+			   argv[0]);
+		return 1;
+	}
+
+	if (runs < 1) {
+		fprintf(stderr, "Invalid number of runs: %d\n", runs);
+		return 2;
+	}
+
+	srand(time(NULL));
+
+	size_t lens[8] = {1, 8, 32, 64, 128, 256, 512, 8196};
+	bench_item *items[8];
+	bench_item *extra_items[8];
+	bench_item *null_items[8];
+	for (int i = 0; i < 8; i++) {
+		items[i] = malloc(sizeof(bench_item) * lens[i]);
+		extra_items[i] = malloc(sizeof(bench_item) * lens[i]);
+		while (extra_items[i] - items[i] < 10000) {
+			extra_items[i] = malloc(sizeof(bench_item) * lens[i]);
+		}
+		null_items[i] = NULL;
+		for (size_t j = 0; j < lens[i]; j++) {
+			items[i][j].id = j;
+			items[i][j].value = rand();
+		}
+	}
+
+	union function_option func;
+	func.extra_bench_func = &benchmark_with_external;
+	run_benchmark("EXTERNAL", lens, items, extra_items, func);
+	func.bench_func = benchmark_with_malloc;
+	run_benchmark("MALLOC", lens, items, null_items, func);
+	func.bench_func = benchmark_with_dynamic;
+	run_benchmark("DYNAMIC", lens, items, null_items, func);
+	func.bench_func = benchmark_with_stack;
+	run_benchmark("STACK", lens, items, null_items, func);
+}
---
+++ src/parallel/parse.py
@@ -0,0 +1,156 @@
+import csv
+
+# taking in some collection of values, leave only 4 or 5 inner values,
+# removing outliers depending on whether the source list was even or odd
+def dump_outliers(values):
+    length = len(values)
+    if length < 5:
+        return values
+
+    target_len = 4 + (length % 2)
+    to_drop = int((length - target_len) / 2)
+    return values[to_drop:(length - to_drop)]
+
+
+# parse a csv into the following dict
+# { "alloc_type": # Dynamic, stack, etc
+#   { "item_count": # 1, 8, 16, etc
+#     [ <times> ]
+#   }
+# }
+def process_csv(csv):
+    results = {}
+    for row in csvs[olevel]:
+        memtype = row[0]
+        size = row[1]
+        time = float(row[2])
+        ops = float(row[3])
+
+        if memtype not in results:
+            results[memtype] = {}
+
+        if size not in results[memtype]:
+            results[memtype][size] = []
+
+        results[memtype][size].append(ops / time)
+
+    return results
+
+
+# compare the results to our predictions
+def score_results(count, stats):
+    def dump_proportion(stat):
+        return list(map((lambda x: x[1]), stat))
+
+    def score_deviation(target, results):
+        results = list(filter((lambda x: x[1] != "EXTERNAL"), results))
+        target["deviation"] = {}
+        target["deviation"]["max"] = (1 - (results[-1][0]/results[0][0])) * 100
+        target["deviation"]["min"] = (1 - (results[1][0]/results[0][0])) * 100
+        deviations = []
+        for i in range(1, len(results)):
+            deviations.append(results[i][0] / results[i - 1][0])
+        target["deviation"]["avg"] = (sum(deviations) / len(deviations) - 1) * 100
+
+    # external and stack should have no overhead and always be fastest
+    # when dynamic chooses to use the stack, it should be faster than malloc
+    def score_with_stack_use(target, results):
+        score_deviation(target, results)
+        results = dump_proportion(results)
+        if results[0] == "STACK":
+            target["score"] = 2
+        elif results[1] == "DYNAMIC":
+            target["score"] = 1
+        else:
+            target["score"] = 0
+
+    # when dynamic chooses not to use the stack, it should perform the same as
+    # or worse than malloc
+    def score_without_stack_use(target, results):
+        score_deviation(target, results)
+        results = dump_proportion(results)
+        if results[0] == "STACK":
+            target["score"] = 2
+        elif results[1] == "MALLOC":
+            target["score"] = 1
+        else:
+            target["score"] = 0
+
+    # if there are 64 or less items (512 bytes), the dynamic method will use
+    # the stack
+    count = int(count)
+    results = {"size": count, "min": {}, "avg": {}, "max": {}}
+    target_func = None
+    if count <= 64:
+        target_func = score_with_stack_use
+    else:
+        target_func = score_without_stack_use
+
+    target_func(results["min"], stats["mins"])
+    target_func(results["avg"], stats["avgs"])
+    target_func(results["max"], stats["maxs"])
+
+    return results
+
+
+csvs = {}
+
+# read in the 4 different results files for the various optimisation levels
+for i in range(0, 2):
+    csvs["O{}".format(i)] = csv.reader(open("resultsO{}.csv".format(i)))
+
+results = {}
+
+# parse out all the CSVs into one dict
+for olevel in csvs:
+    results[olevel] = process_csv(csvs[olevel])
+
+computed = {}
+
+# transform the preceding dict into
+# { "olevel":
+#   { "item_count":
+#     {
+#       "avgs": [ (<time>, "alloc_type"), ... ],
+#       "mins": [ (<time>, "alloc_type"), ... ],
+#       "maxs": [ (<time>, "alloc_type"), ... ]
+#     }
+#   }
+# }
+# however, when calculating the avgs/mins/maxs, dump the outliers first
+for olevel in results:
+    computed[olevel] = {}
+    for memtype in results[olevel]:
+        for i_count in results[olevel][memtype]:
+            i_stats = None
+            if i_count not in computed[olevel]:
+                i_stats = {
+                    "mins": [],
+                    "avgs": [],
+                    "maxs": []
+                }
+                computed[olevel][i_count] = i_stats
+            else:
+                i_stats = computed[olevel][i_count]
+
+            current = dump_outliers(sorted(results[olevel][memtype][i_count], reverse=True))
+            i_stats["avgs"].append((sum(current) / len(current), memtype))
+            i_stats["maxs"].append((max(current), memtype))
+            i_stats["mins"].append((min(current), memtype))
+
+# produce sorted lists indicating the relative performance of each method
+# the first item in each list was the fastest and so forth
+for olevel in computed:
+    for i_count in computed[olevel]:
+        for category in computed[olevel][i_count]:
+            results = sorted(computed[olevel][i_count][category], reverse=(category != "mins"))
+            computed[olevel][i_count][category] = list(map((lambda x: [x[0], x[1]]), results))
+
+# score everything using our scoring function above
+scored = {}
+for olevel in computed:
+    scored[olevel] = []
+    for item_count in computed[olevel]:
+        scored[olevel].append(score_results(item_count, computed[olevel][item_count]))
+
+print({"scored": scored, "computed": computed})
