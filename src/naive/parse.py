a00cb51975b62096f419084e6770337a0ee2ec86
Dário Tavares Antunes <dario@ntun.es>
2018-03-21 12:15:00 +0000

    Modify parse script to output graphable data

--- src/naive/parse.py
+++ src/naive/parse.py
@@ -35,65 +35,6 @@ def process_csv(csv):
     return results
 
 
-# compare the results to our predictions
-def score_results(count, stats):
-    def dump_proportion(stat):
-        return list(map((lambda x: x[1]), stat))
-
-    def score_deviation(target, results):
-        target["deviation"] = {}
-        target["deviation"]["max"] = (results[-1][0] - 1) * 100
-        target["deviation"]["min"] = (results[1][0] - 1) * 100
-        deviations = []
-        for i in range(1, len(results)):
-            deviations.append(results[i][0] / results[i - 1][0])
-        target["deviation"]["avg"] = (sum(deviations) / len(deviations) - 1) * 100
-
-    # external and stack should have no overhead and always be fastest
-    # when dynamic chooses to use the stack, it should be faster than malloc
-    def score_with_stack_use(target, results):
-        score_deviation(target, results)
-        results = dump_proportion(results)
-        if results == ["EXTERNAL", "STACK", "DYNAMIC", "MALLOC"]:
-            target["score"] = 2
-        elif results[3] == "MALLOC":
-            target["score"] = 1
-        elif results[2:] == ["MALLOC", "DYNAMIC"]:
-            target["score"] = 0.5
-        else:
-            target["score"] = 0
-
-    # when dynamic chooses not to use the stack, it should perform the same as
-    # or worse than malloc
-    def score_without_stack_use(target, results):
-        score_deviation(target, results)
-        results = dump_proportion(results)
-        if results == ["EXTERNAL", "STACK", "MALLOC", "DYNAMIC"]:
-            target["score"] = 2
-        elif results[3] == "DYNAMIC":
-            target["score"] = 1
-        elif results[2:] == ["DYNAMIC", "MALLOC"]:
-            target["score"] = 0.5
-        else:
-            target["score"] = 0
-
-    # if there are 64 or less items (512 bytes), the dynamic method will use
-    # the stack
-    count = int(count)
-    results = {"size": count, "min": {}, "avg": {}, "max": {}}
-    target_func = None
-    if count <= 64:
-        target_func = score_with_stack_use
-    else:
-        target_func = score_without_stack_use
-
-    target_func(results["min"], stats["mins"])
-    target_func(results["avg"], stats["avgs"])
-    target_func(results["max"], stats["maxs"])
-
-    return results
-
-
 csvs = {}
 
 # read in the 4 different results files for the various optimisation levels
@@ -139,19 +80,30 @@ for olevel in results:
             i_stats["maxs"].append((max(current), memtype))
             i_stats["mins"].append((min(current), memtype))
 
-# produce sorted lists indicating the relative performance of each method
-# the first item in each list was the fastest and so forth
+
 for olevel in computed:
+    writer = csv.writer(open("processed{}.csv".format(olevel), "w"))
+    writer.writerow(["Items", "External Min", "Stack Min", "Dynamic Min", "External Avg", "Stack Avg", "Dynamic Avg", "External Max", "Stack Max", "Dynamic Max"])
+
     for i_count in computed[olevel]:
-        for category in computed[olevel][i_count]:
-            results = sorted(computed[olevel][i_count][category])
-            computed[olevel][i_count][category] = list(map((lambda x: [x[0]/results[0][0], x[1]]), results))
+        row = [i_count]
+        for category in ["mins", "avgs", "maxs"]:
+            external = None
+            stack = None
+            dynamic = None
+            malloc = None
 
-# score everything using our scoring function above
-scored = {}
-for olevel in computed:
-    scored[olevel] = []
-    for item_count in computed[olevel]:
-        scored[olevel].append(score_results(item_count, computed[olevel][item_count]))
+            for time_tuple in computed[olevel][i_count][category]:
+                if time_tuple[1] == "EXTERNAL":
+                    external = time_tuple[0]
+                if time_tuple[1] == "STACK":
+                    stack = time_tuple[0]
+                if time_tuple[1] == "DYNAMIC":
+                    dynamic = time_tuple[0]
+                if time_tuple[1] == "MALLOC":
+                    malloc = time_tuple[0]
 
-print({"scored": scored, "computed": computed})
+            row.append(external / malloc)
+            row.append(stack / malloc)
+            row.append(dynamic / malloc)
+        writer.writerow(row)
--- src/parallel/parse.py
+++ src/parallel/parse.py
@@ -37,62 +37,6 @@ def process_csv(csv):
     return results
 
 
-# compare the results to our predictions
-def score_results(count, stats):
-    def dump_proportion(stat):
-        return list(map((lambda x: x[1]), stat))
-
-    def score_deviation(target, results):
-        results = list(filter((lambda x: x[1] != "EXTERNAL"), results))
-        target["deviation"] = {}
-        target["deviation"]["max"] = (1 - (results[-1][0]/results[0][0])) * 100
-        target["deviation"]["min"] = (1 - (results[1][0]/results[0][0])) * 100
-        deviations = []
-        for i in range(1, len(results)):
-            deviations.append(results[i][0] / results[i - 1][0])
-        target["deviation"]["avg"] = (sum(deviations) / len(deviations) - 1) * 100
-
-    # external and stack should have no overhead and always be fastest
-    # when dynamic chooses to use the stack, it should be faster than malloc
-    def score_with_stack_use(target, results):
-        score_deviation(target, results)
-        results = dump_proportion(results)
-        if results[0] == "STACK":
-            target["score"] = 2
-        elif results[1] == "DYNAMIC":
-            target["score"] = 1
-        else:
-            target["score"] = 0
-
-    # when dynamic chooses not to use the stack, it should perform the same as
-    # or worse than malloc
-    def score_without_stack_use(target, results):
-        score_deviation(target, results)
-        results = dump_proportion(results)
-        if results[0] == "STACK":
-            target["score"] = 2
-        elif results[1] == "MALLOC":
-            target["score"] = 1
-        else:
-            target["score"] = 0
-
-    # if there are 64 or less items (512 bytes), the dynamic method will use
-    # the stack
-    count = int(count)
-    results = {"size": count, "min": {}, "avg": {}, "max": {}}
-    target_func = None
-    if count <= 64:
-        target_func = score_with_stack_use
-    else:
-        target_func = score_without_stack_use
-
-    target_func(results["min"], stats["mins"])
-    target_func(results["avg"], stats["avgs"])
-    target_func(results["max"], stats["maxs"])
-
-    return results
-
-
 csvs = {}
 
 # read in the 4 different results files for the various optimisation levels
@@ -121,36 +65,43 @@ computed = {}
 for olevel in results:
     computed[olevel] = {}
     for memtype in results[olevel]:
-        for i_count in results[olevel][memtype]:
-            i_stats = None
-            if i_count not in computed[olevel]:
-                i_stats = {
-                    "mins": [],
-                    "avgs": [],
-                    "maxs": []
-                }
-                computed[olevel][i_count] = i_stats
-            else:
-                i_stats = computed[olevel][i_count]
+        if memtype != "EXTERNAL":
+            for i_count in results[olevel][memtype]:
+                i_stats = None
+                if i_count not in computed[olevel]:
+                    i_stats = {
+                        "mins": [],
+                        "avgs": [],
+                        "maxs": []
+                    }
+                    computed[olevel][i_count] = i_stats
+                else:
+                    i_stats = computed[olevel][i_count]
 
-            current = dump_outliers(sorted(results[olevel][memtype][i_count], reverse=True))
-            i_stats["avgs"].append((sum(current) / len(current), memtype))
-            i_stats["maxs"].append((max(current), memtype))
-            i_stats["mins"].append((min(current), memtype))
+                current = dump_outliers(sorted(results[olevel][memtype][i_count], reverse=True))
+                i_stats["avgs"].append((sum(current) / len(current), memtype))
+                i_stats["maxs"].append((max(current), memtype))
+                i_stats["mins"].append((min(current), memtype))
 
-# produce sorted lists indicating the relative performance of each method
-# the first item in each list was the fastest and so forth
 for olevel in computed:
+    writer = csv.writer(open("processed{}.csv".format(olevel), "w"))
+    writer.writerow(["Items", "Stack/Malloc Min", "Dynamic/Malloc Min", "Stack/Malloc Avg", "Dynamic/Malloc Avg", "Stack/Malloc Max", "Dynamic/Malloc Max"])
+
     for i_count in computed[olevel]:
-        for category in computed[olevel][i_count]:
-            results = sorted(computed[olevel][i_count][category], reverse=(category != "mins"))
-            computed[olevel][i_count][category] = list(map((lambda x: [x[0], x[1]]), results))
+        row = [i_count]
+        for category in ["mins", "avgs", "maxs"]:
+            stack = None
+            dynamic = None
+            malloc = None
 
-# score everything using our scoring function above
-scored = {}
-for olevel in computed:
-    scored[olevel] = []
-    for item_count in computed[olevel]:
-        scored[olevel].append(score_results(item_count, computed[olevel][item_count]))
+            for ops_tuple in computed[olevel][i_count][category]:
+                if ops_tuple[1] == "STACK":
+                    stack = ops_tuple[0]
+                if ops_tuple[1] == "DYNAMIC":
+                    dynamic = ops_tuple[0]
+                if ops_tuple[1] == "MALLOC":
+                    malloc = ops_tuple[0]
 
-print({"scored": scored, "computed": computed})
+            row.append(stack / malloc)
+            row.append(dynamic / malloc)
+        writer.writerow(row)
